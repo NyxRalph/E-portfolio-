a7d5a46b85233d060bfb742ddfc65ba122dcf5ec
Ralph Andy Menz <andyralphmenz@gmail.com>
2023-04-15 23:17:50 +0000

    debuging done

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,5 @@
-// 
-//
-//
-
 function contact(event) {
+
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
@@ -21,11 +18,21 @@ function contact(event) {
         alert(
             'The email service is temporarily unavailable, Please contact me directly on email @ ralphandymenz@gmail.com'
         );
-    })
+    }) 
+}
 
-    function toggleModal() {
-        document.body.classList += ' modal__open';
-        console.log('toggleModal()')
+function toggleModal() {
+    document.body.classList += ' modal__open';
+    console.log('toggleModal()')
+}
+
+let isModalOpen = false
+
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false
+        return document.body.classList.remove('modal__open')
     }
-  
+    isModalOpen = true
+    return document.body.classList += ' modal__open'
 }
