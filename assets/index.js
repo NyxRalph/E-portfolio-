f18395b2b1e956f65df11b24e368215ab6bdaeb8
Ralph Andy Menz <andyralphmenz@gmail.com>
2023-04-18 01:24:59 +0000

    website done

--- assets/index.js
+++ assets/index.js
@@ -52,12 +52,15 @@ function toggleModal(){
 }
 
 function moveBackground(event){
-   const shapes = document.querySelectorAll('.shape')
-   const x = event.clientX * scaleFactor
-   const y = event.clientY * scaleFactor
+    const shapes = document.querySelectorAll('.shape')
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
     
-   for( i = 0 ; i < shapes.length ; ++i ){
-    shapes[i].style.transform = `translate(${x}px ,${y}px)`
+    for( i = 0 ; i < shapes.length ; ++i ){
+        const isOdd = i % 2 !== 0
+        const boolint = isOdd ? -1 : 1
+
+        shapes[i].style.transform = `translate(${x * boolint}px  ,${y * boolint}px)`
 
    }
 
