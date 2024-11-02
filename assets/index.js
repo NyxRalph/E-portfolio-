dc7336d504979bbc29eec5ee4390dca3a4d58b73
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-11-02 23:20:43 +0000

    ...

--- assets/index.js
+++ assets/index.js
@@ -16,7 +16,7 @@ function contact(event) {
 
     event.preventDefault();
     // const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
+    // const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible";
     emailjs
     .sendForm(
