2190f1d6c4dc8e8ea6ddf5a98441180f1daf040c
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-11-28 23:54:24 +0000

    ...

--- assets/index.js
+++ assets/index.js
@@ -65,23 +65,23 @@ function moveBackground(event) {
   }
 }
 
-function moveProject(event) {
-  const project = document.querySelectorAll(".project__wrapper");
-  const scaleFactor = 1;
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
+// function moveProject(event) {
+//   const project = document.querySelectorAll(".project__wrapper");
+//   const scaleFactor = 1;
+//   const x = event.clientX * scaleFactor;
+//   const y = event.clientY * scaleFactor;
 
-  for (let i = 0; i < project.length; ++i) {
+//   for (let i = 0; i < project.length; ++i) {
 
-    const isOdd = i % 2 !== 0;
-    const boolint = isOdd ? -1 : 1;
-    project[i].style.transformStyle = `preserve-3d`;
+//     const isOdd = i % 2 !== 0;
+//     const boolint = isOdd ? -1 : 1;
+//     project[i].style.transformStyle = `preserve-3d`;
 
-    project[i].style.transform = `perspective(650px) rotateX(${
-      y * boolint
-    }deg) rotateY(${x * boolint}deg) scale3d(1, 1, 1)`;
-  }
-}
+//     project[i].style.transform = `perspective(650px) rotateX(${
+//       y * boolint
+//     }deg) rotateY(${x * boolint}deg) scale3d(1, 1, 1)`;
+//   }
+// }
 
 const projects = document.querySelectorAll("div.project__wrapper--bg");
 const scalefacterProject = 1 / 21;
